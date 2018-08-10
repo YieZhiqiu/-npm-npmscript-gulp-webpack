94762780833168dc4a0a00c62ed75497e6052e56
YieZhiqiu <30974111+YieZhiqiu@users.noreply.github.com>
2018-08-10 20:26:15 +0800

    Update top.js

--- scripts/top.js
+++ scripts/top.js
@@ -1,1 +1,16 @@
 
+function goTop(){
+  $(window).scroll(function(e){
+    if($(window).scrollTop()>100)
+      $('.top').fadeIn(1000);
+    else
+      $('.top').fadeOut(1000);
+  });
+}
+$(function(){
+  $('.top').click(function(e){
+    console.log(1)
+    $('body, html').animate({scrollTop: 0},1000);
+  });
+  goTop();
+});
